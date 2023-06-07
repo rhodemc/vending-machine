4528be0cf8827adebe93bc661cab973ba773ef29
rhodemc <rhodemc@gmail.com>
2023-06-06 21:53:19 -0700

    finished todos

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,4 @@
+// dependencies
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,8 +6,10 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// precacheAndRoute is a function that takes an array of files
 precacheAndRoute(self.__WB_MANIFEST);
 
+// pageCache is a strategy
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -19,12 +22,25 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// warmStrategyCache is a function that takes an object with urls and a strategy
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// registerRoute is a function that takes a callback function and a strategy
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// TODO: Implemented asset caching
+registerRoute(
+  ({ request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,33 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // store the triggered event
+    window.deferredPrompt = event;
+    // remove the hidden class from the install button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // check if the deferred prompt event has been stored
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show the install prompt.
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable, since
+    window.deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // clear prompt on install
+    window.deferredPrompt = null;
+});
